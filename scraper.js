b416e3b11392eec3b0ac2c19afc08f24a0a9e0ca
Paul Walker <pw97960@gmail.com>
2018-11-10 09:15:01 -0800

    Change csv to json2csv

--- scraper.js
+++ scraper.js
@@ -5,81 +5,29 @@
 /* REQUIRE STATEMENTS -------------------------------------------------- */
 const request = require('request-promise');
 const cheerio = require('cheerio');
-const csv = require('fast-csv');
 const fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
 
-// This variable calls the scrapeDate function and pre-pend to the csv file.
+/* GLOBAL VARIABLES ----------------------------------------------------- */
 const time = scrapeDate();
+const fields = ['Title','Price','imageURL','URL','time'];
+const folderName = './Data'; 
 
-// This function gets shirts.php page loads cheerio gets links and returns it. 
-(async function() {
+/* SEPERATE FUNCTION STATEMENTS ----------------------------------------- */
+
+// This function checks if there is a folder make new one if not.
+function checkIffolder () {
   try {
-    const entryPoint = 'http://shirts4mike.com/shirts.php';
-    const base = entryPoint.slice(0,23);
-    const mainHtml = await request(entryPoint);
-    const $ = cheerio.load(mainHtml);
-
-    const links = $('.products li a').map((index, element) => {
-      const $url = $(element).attr('href');
-      return {
-        link:`${base}${$url}`,
-        URL: $url
-      }
-  // Have to attach the get function for cheerio to map the links
-    }).get();
-  // This function waits until all promise is completed then moves on to the next task
-    const data = await Promise.all(links.map(async (shirt) => {
-  // Async requires you use the try catch block
-      try {
-        const shirtHtml = await request(shirt.link);
-        const $ = cheerio.load(shirtHtml);
-
-        const Title = $('head title').text();
-        const Price = $('.price').text();
-        const imageURL = $('.shirt-picture img').attr('src');
-        const URL = shirt.URL;
-        const time = new Date();
-
-
-             return {
-              Title,
-              Price,
-              imageURL,
-              URL,
-              time
-            }           
-
-      } catch (e) {
-        return e.message;
-      }
-    }));
-
-    fs.mkdir('Data', () => {
-      const writableStream = fs.createWriteStream(`./Data/${time}.csv`);
-
-      csv.
-      write(data, {headers:true}).pipe(writableStream);
-
-      writableStream.on("finish", function(){
-        console.log("Scraping DONE!");
-      });
-    })
-
-    // console.log(data);
-  } catch (e) {
-    // If error is thrown the dateTime is written (errorMessageFile) to the scraper-error log
-    const dateTime = new Date() + '<errorMessage>';
-    const errorMessageFile = fs.writeFile('scraper-error.log', `${dateTime}`, (e) => {
-      if (e) {
-          console.log('Error while writing to file', e);
-      }
-    });
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName)
+    }
+  } catch (err) {
+    console.error(err)
   }
+}
 
-})();
-
-// This function formats the date and removes the time from the date method
-function scrapeDate() {
+// This function formats the time for file name
+function scrapeDate () {
   let date = new Date();
   let year = date.getFullYear().toString();
   let month = (date.getMonth() + 1).toString();
@@ -90,3 +38,75 @@ function scrapeDate() {
   return yyyymmdd;
 }
 
+// This function will write out errors to the scraper-error log
+// Function gets called in the catch statement if there is an error
+function errorMessageFn () {
+    const dateTime = new Date() + '<errorMessage>';
+    const errorMessageFile = fs.writeFileSync('scraper-error.log', 
+								`${dateTime}\n`, {flag: 'a'}, (e) => {
+      if (e) {
+          console.log('Error while writing to file', e);
+      }
+    });
+}
+
+
+/* WEB SCRAPER FUNCTION ---------------------------------------------------- */
+
+// This function gets entryPoint, loads cheerio, gets links and returns it. 
+// The base removes the shirts.php. I await response then call the entryPoint.
+(async function() {
+	try {
+		const entryPoint = 'http://shirts4mike.com/shirts.php';
+	    const base = entryPoint.slice(0,23);
+	    const mainHtml = await request(entryPoint);
+	    const $ = cheerio.load(mainHtml);
+	    const links = $('.products li a').map((index, element) => {
+      	const $url = $(element).attr('href');
+	      	return {
+		        	link:`${base}${$url}`,
+		        	URL: $url
+	  			}
+  // Have to attach the get function or you get a cheerio array
+    }).get();
+
+	    const data = await Promise.all(links.map(async (shirt) => {
+	    	try {
+
+	    		const shirtHtml = await request(shirt.link);
+	    		const $ = cheerio.load(shirtHtml);
+	    		const Title = $('head title').text();
+		        const Price = $('.price').text();
+		        const imageURL = `${base}` + $('.shirt-picture img').attr('src');
+		        const URL = shirt.link;
+		        const time = new Date();
+
+		        return {
+				          Title,
+				          Price,
+				          imageURL,
+				          URL,
+				          time
+				        } 
+
+	    	}
+
+    	catch (err) {
+    		console.error(err);
+    		errorMessageFn();
+    	}
+
+	    }));
+  			  checkIffolder ();
+			  const json2csvParser = new Json2csvParser({ fields });
+			  const csv = json2csvParser.parse(data);
+			  const writableStream = fs.writeFileSync(`./Data/${time}.csv`, csv); 
+			  console.log('Done!');
+
+	} 
+	catch (err) {
+		console.log('Our error', err);
+		errorMessageFn();
+	}
+
+}());
